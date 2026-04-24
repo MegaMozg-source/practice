2791ec0adb1bbe8510209acb7f50b701a4585b9c
VasilenkoPS <vasilenkops@mer.ci.nsu.ru>
2026-04-24 12:30:11 +0700

    Таск 6

--- main/src/main/java/ci/nsu/moble/main/data/NetworkLayer.kt
+++ main/src/main/java/ci/nsu/moble/main/data/NetworkLayer.kt
@@ -1,5 +1,6 @@
 package ci.nsu.mobile.main.data
 
+import ci.nsu.moble.main.data.dto.UserDto
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.Interceptor
